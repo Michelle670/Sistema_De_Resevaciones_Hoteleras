c0cca9d645224c81914da2702ee6a270fada2150
Nixon <nixonvv02@gmail.com>
2025-12-06 19:57:08 -0600

    Plan Lealtad y Pais Listo

--- src/goHotel/view/GestionPlanLealtad.java
+++ src/goHotel/view/GestionPlanLealtad.java
@@ -13,7 +13,7 @@ import java.awt.event.ActionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-//******************************************************************************
+//*****************************************************************************
 /**
  * AUTOR: GRUPO 3 PROYECTO SEMANA 9
  */
